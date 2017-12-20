c936f6ca1aa78c9bbfc04eef3e81a4b36de90e05
Antonio <tonymike20@hotmail.com>
2017-12-19 20:55:53 -0600

    Se actualizo el body component con directivas estructurales

--- src/app/components/body.component.ts
+++ src/app/components/body.component.ts
@@ -6,4 +6,13 @@ import  { Component } from '@angular/core';
 })
 export class BodyComponent {
 
+  mostrar:boolean = false;
+
+  frase:any = {
+    mensaje: "Un gran poder requiere una gran responsabilidad",
+    autor: "Ben Parker"
+  }
+
+  personajes:string[]  = ["Piccolo", "Vegeta", "Freezer", "Rapisu (Androide No.17)", "Rochi", "Goku", "Tenshinhan", "Gohan", "Kuririn", "Razuri (Androide No.18)"]
+
 }
